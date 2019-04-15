feac9ccdd8df1fb5130ae18b5ea62ab98e211116
Israel dela Cruz <igdcruz42@gmail.com>
2019-04-14 22:24:45 -0500

    added LoginAsyncTask

--- app/src/main/java/com/example/israel/build_week_1_bookr/activity/MainActivity.java
+++ app/src/main/java/com/example/israel/build_week_1_bookr/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.israel.build_week_1_bookr;
+package com.example.israel.build_week_1_bookr.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.israel.build_week_1_bookr.R;
 
 public class MainActivity extends AppCompatActivity {
 
