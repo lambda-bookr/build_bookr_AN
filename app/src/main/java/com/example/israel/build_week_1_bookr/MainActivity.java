e31b8c7184998e57181ad642ef7b61bd0aa8d821
Israel dela Cruz <igdcruz42@gmail.com>
2019-04-14 22:23:08 -0500

    project created

---
+++ app/src/main/java/com/example/israel/build_week_1_bookr/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.israel.build_week_1_bookr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
